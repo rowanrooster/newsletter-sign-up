d796cc0065fe2a0b29de2f1eb28818adca22419d
rowanrooster <pixelphire.rowan@gmail.com>
2024-06-25 14:37:43 +0200

    Final changes

--- dist/script.js
+++ dist/script.js
@@ -6,32 +6,41 @@ const dismissBtn = document.getElementById("dismiss-btn");
 
 const errorMsg = document.getElementById("error-msg");
 const resultMsg = document.getElementById("result-msg");
+const successMsg = document.getElementById("success-msg");
 
+// check input
 emailInput.addEventListener("input", (e) => {
     if (emailInput.validity.valid) {
         errorMsg.textContent = "";
+        emailInput.classList.remove("error");
     } else {
         showError();
     }
 });
 
+// check input on submit
 signupForm.addEventListener("submit", (e) => {
     if (!emailInput.validity.valid || emailInput.validity.valueMissing) {
       showError();
       e.preventDefault();
     } 
+
+    // show success message if valid input
     if(emailInput.validity.valid) {
         e.preventDefault();
         signupContainer.style.display = "none";
         resultMsg.style.display = "flex";
+        let emailMsg = emailInput.value;
+        successMsg.innerHTML = `A confirmation email has been sent to <strong>${emailMsg}</strong>. Please open it and click the button inside to confirm your subscription.`;
       } 
 });
 
+// show error message
 function showError() {
     if (emailInput.validity.valueMissing) {
-        errorMsg.textContent = "You need to enter an email address.";
+        errorMsg.textContent = "Please enter an email address.";
     } else if (emailInput.validity.typeMismatch) {
-        errorMsg.textContent = "Entered value needs to be an email address.";
+        errorMsg.textContent = "Please enter a valid email address.";
     } else if (emailInput.validity.tooShort) {
         errorMsg.textContent = `Email should be at least ${emailInput.minLength} characters.`;
     }  
@@ -39,6 +48,7 @@ function showError() {
     errorMsg.style.display = "block";
 };
 
+// dismiss success message and return
 dismissBtn.addEventListener("click", () => {
     signupContainer.style.display = "flex";
     resultMsg.style.display = "none";
